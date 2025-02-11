fcdba29472d6cdc98ded55b8cf3a40e1d942db5f
Rinyxx <Kirillmedvedev240306@mail.ru>
2025-02-11 14:24:31 +0300

    files

---
+++ Sgifr_Atbsh_Duchin_Medvedev.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm> // для tolower и toupper
+
+using namespace std;
+
+char atbashChar(char c) {
+    if (isalpha(c)) {
+        char base = isupper(c) ? 'A' : 'a';
+        return base + ('Z' - toupper(c)); // Более эффективное вычисление обратного символа
+    }
+    return c; // Оставляем другие символы без изменений
+}
+
+string atbash(const string& text) {
+    string result = "";
+    for (char c : text) {
+        result += atbashChar(c);
+    }
+    return result;
+}
+
+int main() {
+    setlocale(0, "");
+    string text;
+    cout << "Введите текст для шифрования: ";
+    getline(cin, text);
+
+    string encrypted = atbash(text);
+    cout << "Зашифрованный текст: " << encrypted << "\n";
+    return 0;
+}
